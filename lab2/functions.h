9c480a05ff2b9a35e542aca0d04f16afbe03d83d
Kamil Rojek <kamilrojek2002@gmail.com>
2023-04-04 20:21:46 +0200

    Update functions.h

--- lab2/functions.h
+++ lab2/functions.h
@@ -1,4 +1,13 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+/**
+ * This function performs linear search on an integer array to find the first occurrence of a given value.
+ * @param Array: A pointer to the first element of the integer array to search.
+ * @param array_size: The number of elements in the array.
+ * @param x: The value to search for in the array.
+ * @return If the value is found in the array, the function returns the index of its first occurrence. Otherwise, it returns -1.
+ */
+int linearSearch1(int* Array, int array_size, int x);
+
 #endif
