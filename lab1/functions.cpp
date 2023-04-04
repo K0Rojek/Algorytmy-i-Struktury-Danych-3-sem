94a7aaf119ce805ae2941b75b0aef699f1099d88
Kamil Rojek <kamilrojek2002@gmail.com>
2023-04-04 15:35:00 +0200

    Create functions.cpp

---
+++ lab1/functions.cpp
@@ -0,0 +1,11 @@
+#include"functions.hpp"
+#include<iostream>
+
+int f_div_sum(int number) {
+    int sum = 0;
+    for (int i = 1; i < number; i++) {
+        if (number % i == 0)
+            sum += i;
+    }
+    return sum;
+}
