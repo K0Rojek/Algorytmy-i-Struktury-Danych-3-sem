f96bd6e34db6b334ddbdd12f5b34dc36a9cdfe15
Kamil Rojek <kamilrojek2002@gmail.com>
2023-04-04 20:21:48 +0200

    Update functions.cpp

--- lab2/functions.cpp
+++ lab2/functions.cpp
@@ -2,3 +2,12 @@
 #include <iostream>
 #include <time.h>
 #include <algorithm>
+
+int linearSearch1(int* Array, int array_size, int x) {
+    // If the current element matches the value being searched for, return its index.
+    for (int i = 0; i < array_size; i++)
+        if (Array[i] == x)
+            return i;
+    // If the value is not found in the array, return -1.
+    return -1;
+}
