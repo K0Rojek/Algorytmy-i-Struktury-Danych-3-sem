44f9e52952f11909f5ec7f4d0c61b3d355e6223b
Kamil Rojek <kamilrojek2002@gmail.com>
2023-04-04 15:35:03 +0200

    Create functions.h

---
+++ lab1/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+/**
+ * This function will return sum of all proper divisors of number
+ * @param number number we want to search all divisors proper
+ * @return number of all divisors proper
+ */
+int f_div_sum(int number);
+
+
+#endif
